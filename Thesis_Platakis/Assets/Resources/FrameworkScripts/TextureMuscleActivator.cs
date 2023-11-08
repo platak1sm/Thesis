ec52ad387cdb80c019be42a8e8d29c3ea41cd417
platakism <platakism11@gmail.com>
2023-11-08 19:56:47 +0200

    no message

--- Thesis_Platakis/Assets/Resources/FrameworkScripts/TextureMuscleActivator.cs
+++ Thesis_Platakis/Assets/Resources/FrameworkScripts/TextureMuscleActivator.cs
@@ -15,36 +15,68 @@ public class TextureMuscleActivator : MuscleActivator
     // Start is called before the first frame update
     void Start()
     {
+        jointActions[b.bodyparts["LShoulder"]] = HandleShoulder;
+        jointActions[b.bodyparts["RShoulder"]] = HandleShoulder;
         jointActions[b.bodyparts["LArm"]] = HandleArm;
         jointActions[b.bodyparts["RArm"]] = HandleArm;
         jointActions[b.bodyparts["LForeArm"]] = HandleForeArm;
         jointActions[b.bodyparts["RForeArm"]] = HandleForeArm;
+        jointActions[b.bodyparts["LUpLeg"]] = HandleUpLeg;
+        jointActions[b.bodyparts["RUpLeg"]] = HandleUpLeg;
+        jointActions[b.bodyparts["LLeg"]] = HandleLeg;
+        jointActions[b.bodyparts["RLeg"]] = HandleLeg;
+        jointActions[b.bodyparts["LFoot"]] = HandleFoot;
+        jointActions[b.bodyparts["RFoot"]] = HandleFoot;
+
+    }
+
+    private void HandleShoulder(Transform thresholds)
+    {
+
     }
 
     private void HandleArm(Transform thresholds)
-    {
-        // 98= max intensity, min intensity: 173 for shoulder
+    {   // Vector3(2.00000381, 83.4130096, 73.072998),  Vector3(86.099968,83.4129944,73.0729904) Vector3(82.260994,31.6448059,15.1833076)
+        // 98= max intensity, min intensity: 170 for shoulder
         string jname;
         if (thresholds.gameObject.name == "Skeleton_LeftArm") jname = "LArm";
         else jname = "RArm";
-        Vector3 rotation = b.bodyparts[jname].transform.rotation.eulerAngles;
+        Vector3 rotation = b.bodyparts[jname].transform.localRotation.eulerAngles;
         float distance = thresholds.rotation.eulerAngles.x - rotation.x;
         float distancel = Mathf.InverseLerp(0f, 62f, distance);
         print("Distance 0-1: " + distancel);
         Activate(1 - distancel);
     }
-
+ 
     private void HandleForeArm(Transform thresholds)
     {
+        // inital arm rotation for shoulder press Vector3(27.6879864, 357.107605, 2.22405672) inital forearm rot: Vector3(350.849213,49.7152138,245.169556)
         // 98= max intensity, min intensity: 173 for shoulder
+        // -185= min intensity , max:Vector3(27.6880131,357.108002,286.700012)
         string jname;
         if (thresholds.gameObject.name == "Skeleton_LeftForeArm") jname = "LForeArm";
         else jname = "RForeArm";
-        Vector3 rotation = b.bodyparts[jname].transform.rotation.eulerAngles;
-        float distance = thresholds.rotation.eulerAngles.x - rotation.x;
-        float distancel = Mathf.InverseLerp(0f, 62f, distance);
-        print("Distance 0-1: " + distancel);
-        Activate(1 - distancel);
+        //Vector3 rotation = b.bodyparts[jname].transform.rotation.eulerAngles;
+        //float distance = thresholds.rotation.eulerAngles.x - rotation.x;
+        //float distancel = Mathf.InverseLerp(0f, 62f, distance);
+        //print("Distance 0-1: " + distancel);
+        //Activate(1 - distancel);
+    }
+
+
+    private void HandleUpLeg(Transform thresholds)
+    {
+
+    }
+
+    private void HandleLeg(Transform thresholds)
+    {
+
+    }
+
+    private void HandleFoot(Transform thresholds)
+    {
+
     }
 
     public override void Evaluate(GameObject[] joints, Transform[] thresholds)
@@ -62,7 +94,7 @@ public class TextureMuscleActivator : MuscleActivator
             }
             else
             {
-                Debug.Log("This joint is not yet implemented");
+                Debug.Log("This joint is not yet implemented.");
             }
         }
     }
