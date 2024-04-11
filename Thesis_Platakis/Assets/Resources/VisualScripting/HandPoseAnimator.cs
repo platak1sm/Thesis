3818a25e2ed7b2ebd174dfad2905100909d9ae1e
platakism <platakism11@gmail.com>
2024-04-11 15:43:22 +0300

    Almost ready handposes and ready Mirror

---
+++ Thesis_Platakis/Assets/Resources/VisualScripting/HandPoseAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using MAGES.GameController;
+
+
+
+public class HandPoseAnimator : MonoBehaviour
+{
+    public Animator handAnimator;
+    public string whichHand = "";
+
+    // Update is called once per frame
+    void Update()
+    {
+        handAnimator.SetFloat("Grip_" + whichHand, MAGESControllerClass.Get.GetControllerGrabStrength(MAGESControllerClass.MAGESHand.left));
+        handAnimator.SetFloat("Grip_" + whichHand, MAGESControllerClass.Get.GetControllerGrabStrength(MAGESControllerClass.MAGESHand.right));
+        handAnimator.SetFloat("Trigger_" + whichHand, MAGESControllerClass.Get.GetControllerGrabStrength(MAGESControllerClass.MAGESHand.left));
+        handAnimator.SetFloat("Trigger_" + whichHand, MAGESControllerClass.Get.GetControllerGrabStrength(MAGESControllerClass.MAGESHand.right));
+    }
+
+
+
+}
